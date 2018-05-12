b88f3fc6c96fece0b86cfd1f380c43255623f343
moon <wings0411@gmail.com>
2018-05-12 14:05:12 +0800

    初版

--- theLight/ViewController.swift
+++ theLight/ViewController.swift
@@ -7,9 +7,35 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var count = 0
 
+    @IBAction func pressedButton(_ sender: Any) {
+        count = count + 1
+        let number = count % 2
+        if number == 0 {
+            oneImageView.isHidden = false
+            bonjourLabel.isHidden = false
+            twoImageView.isHidden = true
+            aurevoirLabel.isHidden = true
+        } else if number == 1 {
+            oneImageView.isHidden = true
+            bonjourLabel.isHidden = true
+            twoImageView.isHidden = false
+            aurevoirLabel.isHidden = false
+        } else if count == 2 {
+            count = 0
+        }
+    }
+    
+    @IBOutlet weak var aurevoirLabel: UILabel!
+    @IBOutlet weak var bonjourLabel: UILabel!
+    @IBOutlet weak var twoImageView: UIImageView!
+    @IBOutlet weak var oneImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
